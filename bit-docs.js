ba6dbeb7dbb88f1e345ec4354921684b92260be1
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-07 08:00:05 -0500

    prelim docs

--- bit-docs.js
+++ bit-docs.js
@@ -1,5 +1,19 @@
 var sourceref = require("./sourceref");
 
+/**
+ * @module {function} bit-docs-tag-sourceref
+ * @parent plugins
+ *
+ * @description Adds a `@sourceref` tag for use in `bit-docs`.
+ *
+ * @body
+ *
+ * `@sourceref` will load the file indicated like:
+ *
+ * `@sourceref ./relative/path/something.js`
+ *
+ * And wrap it with triple \` so it shows up as a code block.
+ */
 module.exports=  function(bitDocs) {
     bitDocs.register("tags",{
         sourceref: sourceref
